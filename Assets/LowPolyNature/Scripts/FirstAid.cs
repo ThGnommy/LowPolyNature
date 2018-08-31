3cc64ce498983ba57f8fcc82735e330de3c3463f
Gnommy <th.brandoli@gmail.com>
2018-08-31 10:56:45 +0200

    add first aid use sound

--- Assets/LowPolyNature/Scripts/FirstAid.cs
+++ Assets/LowPolyNature/Scripts/FirstAid.cs
@@ -6,10 +6,12 @@ public class FirstAid : InventoryItemBase
 {
     public int HealthPoints = 20;
     public AudioSource audioSource;
+    public AudioSource FirstAidUse;
 
     private void Start()
     {
        AudioSource audioSource = GetComponent<AudioSource>();
+       AudioSource FirstAidUse = GetComponent<AudioSource>();
     }
 
     public override void OnUse()
@@ -17,6 +19,8 @@ public class FirstAid : InventoryItemBase
         GameManager.Instance.Player.Rehab(HealthPoints);
 
         GameManager.Instance.Player.Inventory.RemoveItem(this);
+
+        FirstAidUse.Play();
 
         Destroy(this.gameObject);
     }
