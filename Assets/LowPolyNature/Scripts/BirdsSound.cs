80b62bc2ba68f438622adf1b217a9d49303933d2
Gnommy <th.brandoli@gmail.com>
2018-09-03 18:11:53 +0200

    remake birds, add gem sounds, fix food pickup

---
+++ Assets/LowPolyNature/Scripts/BirdsSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdsSound : MonoBehaviour {
+
+    public AudioClip[] birds;
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!audioSource.isPlaying)
+        {
+            int n = Random.Range(1, birds.Length);
+            audioSource.clip = birds[n];
+            audioSource.PlayOneShot(audioSource.clip);
+
+            Debug.Log("birds play");
+        }
+    }
+}
--- Assets/LowPolyNature/Scripts/Gem.cs
+++ Assets/LowPolyNature/Scripts/Gem.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Gem : InventoryItemBase {
 
-
+    public AudioSource gemsound;
     public override void OnUse()
     {
         // TODO: Do something with the object.... next tutorial!
         base.OnUse();
+    }
+
+    public override void OnPickup()
+    {
+        base.OnPickup();
+        gemsound.GetComponent<AudioSource>().Play();
     }
 }
